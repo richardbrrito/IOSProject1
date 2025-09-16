f96be6fabd5d76bb8500dd5d9db5e92aa42b203a
Richard Brito <richardbrito116@gmail.com>
2025-09-15 23:17:00 -0400

    Initial Commit

---
+++ Project75/Project75App.swift
@@ -0,0 +1,17 @@
+//
+//  Project75App.swift
+//  Project75
+//
+//  Created by Richard Brito on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Project75App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project75Tests/Project75Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project75Tests.swift
+//  Project75Tests
+//
+//  Created by Richard Brito on 9/15/25.
+//
+
+import Testing
+@testable import Project75
+
+struct Project75Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
